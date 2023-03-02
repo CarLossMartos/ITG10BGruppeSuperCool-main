4435d30bedf402ce01f98e9686843fd7506b9eff
Alexander Grunert <alexander.grunert@vema-eg.de>
2023-03-02 15:26:47 +0100

    (Main Methode) manfred object lastnamee change

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import schueler
 manfred = schueler.Schueler()
 
 manfred.firstname = "Mafred"
-manfred.lastname = "Manfredos"
+manfred.lastname = "Harald"
 manfred.klassenlehrer = "Schergo"
 manfred.date_of_birth = "01.01.2000"
 
