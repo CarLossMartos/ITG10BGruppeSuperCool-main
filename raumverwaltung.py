e2305503ae3db7666ef10ee0074434ae2021dc9f
Alexander Grunert <alexander.grunert@vema-eg.de>
2023-03-20 08:19:45 +0100

    (ProjectTools)Klassen Diagram umgesetzt ~Alex

--- raumverwaltung.py
+++
@@ -1,5 +0,0 @@
-class Raumverwaltung:
-    def __init__(self, raumnr: str, datum: str, klassenlehrer: str):
-        self.raumnr = raumnr
-        self.datum = datum
-        self.klassenlehrer = klassenlehrer
